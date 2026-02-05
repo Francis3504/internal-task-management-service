005a7792ceadd23ba5577282dc497aab4324c19a
unknown <Mbewef86@gmail.com>
2026-02-04 18:53:13 -0800

    Refactoring code and creating proper project structure

--- database/db.py
+++ database/db.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-from core.url import DATABASE_URL
 
-engine=create_engine(DATABASE_URL,echo=True)
+DATABASE_URL=os.getenv("DATABASE_URL","postgresql://user:pass@localhost/task_manager_db")
+engine=create_engine(DATABASE_URL,echo=True,pool_pre_ping=True)
 
 Session=sessionmaker(expire_on_commit=False,autoflush=False,bind=engine)
 
